40328e6b78b9670872c21cbe0325d0b1ae993628
hanadasuka <hanadasuka@gmail.com>
2024-03-15 10:06:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Form from './components/Form';
+import Title from './components/Title';
+import axios from 'axios'
+import Results from './components/Results';
 
 function App() {
+  const [word, setWord] = useState("");
+  const [photo, setPhoto] = useState([]); 
+  const getPhotoData = (e) => {
+    e.preventDefault();
+    axios
+    .get(`https://api.unsplash.com//search/photos?query=${word}&client_id=zkeq-96g5n22Nt9MTsOzr6Xamv2yqQQbj3ceOxNuxNQ`)
+    .then(res => {
+      setPhoto(res.data.results)
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title/>
+      <Form setWord = {setWord} getPhotoData = {getPhotoData}/>
+      <Results photo={photo}/>
     </div>
   );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <header>
+        <h1>Image Search App</h1>
+        <p>By <a href="https://unsplash.com">Unsplash</a></p>
+    </header>
+  )
+}
+export default Title
